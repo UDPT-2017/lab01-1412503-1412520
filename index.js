9f0804ddd5fb15b60d09b26c92250a5a517d52c1
thaonguyen1096 <thaonguyen1096@gmail.com>
2017-04-16 12:47:53 +0700

    edit port

--- index.js
+++ index.js
@@ -166,6 +166,6 @@ app.get('/album-detail', function(req, res){
                       });
   });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Example app listening!');
 })
