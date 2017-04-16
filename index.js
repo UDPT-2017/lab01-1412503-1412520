4b08ecefef3e78d7437ad496bc5a6e41f2b4314c
thikhin96 <nghoangthi1@gmail.com>
2017-04-16 23:42:41 +0700

    edit

--- index.js
+++ index.js
@@ -197,6 +197,6 @@ app.get('/albums/:albumid/:picid', function(req, res){
 
 
 
-app.listen(3000, function () {
+app.listen(process.env.PORT||3000, function () {
   console.log('Example app listening on port 3000!');
 });
